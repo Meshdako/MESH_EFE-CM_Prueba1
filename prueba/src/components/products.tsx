56315984eb3eb9adcd3986769f8e3846fd707c16
Meshdako <msobarzo@utem.cl>
2023-10-30 23:24:08 -0300

    Producto mostrados correctamente

--- prueba/src/components/products.tsx
+++ prueba/src/components/products.tsx
@@ -2,14 +2,29 @@ import React from "react";
 import { getFakestore } from "../services/fakestore.service";
 import { FakeStore, Data, Rating } from "../interfaces/fakestore.interface"
 
+interface Iprops {
+    data: Data[]
+}
 
-
-function Products({data}: FakeStore) {
+function Products({ data }: Iprops) {
 
     return ( 
         <div>
             <h1>Products</h1>
-            
+            <table>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Precio</th>
+                </tr>
+                {
+                    data.map((product) => (
+                        <tr key={product.id}>
+                            <td>{product.title}</td>
+                            <td>{product.price}</td>
+                        </tr>
+                    ))
+                }
+            </table>
         </div>
     );
 }
