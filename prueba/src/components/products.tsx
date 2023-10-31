e50ab63d4b50c10e79fb7d9d4794d0ec035f7f6c
Meshdako <msobarzo@utem.cl>
2023-10-30 21:34:55 -0300

    Carpeta de componentes creada

---
+++ prueba/src/components/products.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { getFakestore } from "../services/fakestore.service";
+import { FakeStore, Data, Rating } from "../interfaces/fakestore.interface"
+
+
+
+function Products({data}: FakeStore) {
+
+    return ( 
+        <div>
+            <h1>Products</h1>
+            
+        </div>
+    );
+}
+
+export default Products;
