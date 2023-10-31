0cee1aa3b4ecca426575e8b165ebc5827284d3ce
Meshdako <msobarzo@utem.cl>
2023-10-30 23:35:30 -0300

    Pantalla de carga cada vez que se presiona el botón

--- prueba/src/containers/App.tsx
+++ prueba/src/containers/App.tsx
@@ -4,33 +4,42 @@ import '../App.css';
 import Products from '../components/products';
 import { FakeStore, Data, Rating } from '../interfaces/fakestore.interface';
 import { getFakestore } from '../services/fakestore.service';
-import { get } from 'http';
+import { Loading } from '../components/loding';
 
 export const App = () => {
   // Declaramos los estados.
   const [productos, setProductos] = useState<Data[]>([])
+  const [loading, setLoading] = useState<boolean>(false)
 
   const getDataAsync = async () => {
+    setLoading(true)
     getFakestore()
       .then((response) => {
         const data = response.data as Data[]
         setProductos(data)
-        console.log(data.map((product) => product.title))
+        setLoading(false)
       })
       .catch((error) => {
         console.log(error)
       })
+
+    await new Promise((resolve) => setTimeout(resolve, 7000));
+    
     console.log("Productos!")
+    setLoading(false)
   }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <button onClick={getDataAsync}>Obtener Productos</button>
-        <Products data={productos} />
-      </header>
+      {
+        loading ? <Loading /> : (
+          <header className="App-header">   
+          <br />         
+            <button onClick={getDataAsync}>Obtener Productos</button>
+            <Products data={productos} />
+          </header>
+        )
+      }
     </div>
   );
 }
