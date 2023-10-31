7d213f5159f6eaf28bedb8524c0df7ddbebcc122
Meshdako <msobarzo@utem.cl>
2023-10-30 21:35:34 -0300

    getFakeStore funcionando

--- prueba/src/containers/App.tsx
+++ prueba/src/containers/App.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
+import Products from '../components/products';
+import { FakeStore, Data, Rating } from '../interfaces/fakestore.interface';
+import { getFakestore } from '../services/fakestore.service';
 
-function App() {
+export const App = () => {
+  // Declaramos los estados.
+  const [products, setProducts] = useState([])
+
+  const getDataAsync = async () => {
+    getFakestore()
+      .then((response) => {
+        const data  = response.data as Data
+
+        console.log(data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    console.log("Productos!")
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>HELLO WORLD!</h1>
-
+        
+        <button onClick={getDataAsync}>Get Products</button>
       </header>
     </div>
   );
 }
-
-export default App;
